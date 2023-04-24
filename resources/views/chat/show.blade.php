0eb1b3d897914301c13e7fd4a210016ab5216f81
Behruz Muyassarov <muyassarov@gmail.com>
2023-04-24 18:01:08 +0500

    feat: chat application view file

---
+++ resources/views/chat/show.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.app')
+
+@push('styles')
+    <style>
+    </style>
+@endpush
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">{{ __('Chat') }}</div>
+
+                    <div class="card-body">
+                        <div class="row p-2">
+                            <div class="col-10">
+                                <div class="row">
+                                    <div class="col-12 border rounded-4 p-3">
+                                        <ul class="list-unstyled overflow-auto" id="messages" style="height: 45vh">
+                                            <li>Test1: Hello</li>
+                                            <li>Test2: Hello</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <form action="">
+                                    <div class="row py-3">
+                                        <div class="col-10">
+                                            <input id="message" type="text" class="form-control"/>
+                                        </div>
+                                        <div class="col-2">
+                                            <button type="submit" id="send-btn" class="btn btn-primary w-100">Send</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="col-2">
+                                <p><strong>Online Now</strong></p>
+                                <ul id="users" class="list-unstyled overflow-auto text-info" style="height: 45vh;">
+                                    <li>Test1</li>
+                                    <li>Test2</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('scripts')
+    <script></script>
+@endpush
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChatController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,3 +25,5 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::view('/users', 'users.showAll')->name('users.all');
 
 Route::view('/game', 'game.show')->name('game.show');
+
+Route::get('/chat', [ChatController::class, 'showChat'])->name('chat.show');
